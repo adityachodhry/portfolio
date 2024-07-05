cb0fe4319ddf0bb1971de4a0ac5cb79c02f92689
Aditya Choudhary <adityachy02131@gmail.com>
2024-07-05 09:24:01 +0530

    First html content

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
+    # return render(request, 'index.html')
     return HttpResponse('This is Home Page!')
 
 def about(request):
